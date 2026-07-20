819d002eb6fb18b608a929e166b50c1a7614461c
lucians <lucians@ensemble.com>
2026-07-20 10:24:55 +0300

    add login and register functionality

--- components/auth-guard/AuthGuard.tsx
+++ components/auth-guard/AuthGuard.tsx
@@ -2,6 +2,7 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
+import { AppPaths } from "@/enums/AppPaths";
 
 export default async function AuthGuard({
     children,
@@ -12,10 +13,8 @@ export default async function AuthGuard({
         headers: await headers(),
     });
 
-    console.log(session)
-
     if (!session) {
-        redirect("/login");
+        redirect(AppPaths.LOGIN);
     }
 
     return <>{children}</>;
